739a6bd66ba843b3288132da65f95187dcf2e0b2
Benhochoi <denousce@gmail.com>
2025-08-05 00:29:29 +0700

    Add files via upload

--- Database.sql
+++ Database.sql
@@ -1,88 +1,90 @@
 CREATE DATABASE csdl_bit;
-
+drop database csdl_bit
 CREATE TABLE csdl_bit.Users (
-  id_user char(10) PRIMARY KEY,
-  username varchar(255),
-  email varchar(255),
-  phone char(10),
+  id varchar(50) PRIMARY KEY,
+  username varchar(50),
+user_password varchar(225),
+  email varchar(50),
+  phone varchar(50),
   age integer,
-  department varchar(255)
+  department varchar(50)
 );
 
 CREATE TABLE csdl_bit.Documents (
-  id_document char(10) PRIMARY KEY,
-  title varchar(255),
+  document_id varchar(50) PRIMARY KEY,
+  title varchar(50),
   content text COMMENT 'Content of the post',
-  uploaded_by char(10)
+  password text,
+  uploaded_by varchar(50)
 );
 
 CREATE TABLE csdl_bit.Tags (
-  id_tag char(10) PRIMARY KEY,
-  name varchar(255)
+  tag_id varchar(50) PRIMARY KEY,
+  nameposts_tags  varchar(255)
 );
 
 CREATE TABLE csdl_bit.Posts (
-  id_post char(10) PRIMARY KEY,
+  post_id varchar(50) PRIMARY KEY,
   title text,
   content text,
-  posted_by char(10)
+  posted_by varchar(50)
 );
 
 CREATE TABLE csdl_bit.Ratings (
-  id_rating char(10) PRIMARY KEY,
+  rating_id varchar(50) PRIMARY KEY,
   star_rating int,
-  rated_by char(10),
-  id_document char(10)
+  rated_by varchar(50),
+  document_id varchar(50)
 );
 
 CREATE TABLE csdl_bit.Comments (
-  id_comment char(10) PRIMARY KEY,
+  comment_id varchar(50) PRIMARY KEY,
   content text,
-  commented_by char(10),
-  id_document char(10),
-  id_post char(10)
+  commented_by varchar(50),
+  post_id varchar(50),
+  document_id varchar(50)
 );
 
 CREATE TABLE csdl_bit.Posts_tags (
-  id_post char(10),
-  id_tag char(10),
-  PRIMARY KEY (id_post, id_tag)
+  post_id varchar(50),
+  tag_id varchar(50),
+  PRIMARY KEY (post_id, tag_id)
 );
 
 CREATE TABLE csdl_bit.Documents_tags (
-  id_document char(10),
-  id_tag char(10),
-  PRIMARY KEY (id_document, id_tag)
+  document_id varchar(50),
+  tag_id varchar(50),
+  PRIMARY KEY (document_id, tag_id)
 );
 
 CREATE TABLE csdl_bit.Bookmarks (
-  id_document char(10),
-  id_user char(10),
-  PRIMARY KEY (id_document, id_user)
+  document_id varchar(50),
+  user_id varchar(50),
+  PRIMARY KEY (document_id, user_id)
 );
 
-ALTER TABLE csdl_bit.Documents ADD FOREIGN KEY (uploaded_by) REFERENCES csdl_bit.Users (id_user);
+ALTER TABLE csdl_bit.Documents ADD FOREIGN KEY (uploaded_by) REFERENCES csdl_bit.Users (id);
 
-ALTER TABLE csdl_bit.Posts ADD FOREIGN KEY (posted_by) REFERENCES csdl_bit.Users (id_user);
+ALTER TABLE csdl_bit.Posts ADD FOREIGN KEY (posted_by) REFERENCES csdl_bit.Users (id);
 
-ALTER TABLE csdl_bit.Ratings ADD FOREIGN KEY (rated_by) REFERENCES csdl_bit.Users (id_user);
+ALTER TABLE csdl_bit.Ratings ADD FOREIGN KEY (rated_by) REFERENCES csdl_bit.Users (id);
 
-ALTER TABLE csdl_bit.Ratings ADD FOREIGN KEY (id_document) REFERENCES csdl_bit.Documents (id_document);
+ALTER TABLE csdl_bit.Ratings ADD FOREIGN KEY (document_id) REFERENCES csdl_bit.Documents (document_id);
 
-ALTER TABLE csdl_bit.Comments ADD FOREIGN KEY (commented_by) REFERENCES csdl_bit.Users (id_user);
+ALTER TABLE csdl_bit.Comments ADD FOREIGN KEY (commented_by) REFERENCES csdl_bit.Users (id);
 
-ALTER TABLE csdl_bit.Comments ADD FOREIGN KEY (id_document) REFERENCES csdl_bit.Documents (id_document);
+ALTER TABLE csdl_bit.Comments ADD FOREIGN KEY (document_id) REFERENCES csdl_bit.Documents (document_id);
 
-ALTER TABLE csdl_bit.Comments ADD FOREIGN KEY (id_post) REFERENCES csdl_bit.Posts (id_post);
+ALTER TABLE csdl_bit.Comments ADD FOREIGN KEY (post_id) REFERENCES csdl_bit.Posts (post_id);
 
-ALTER TABLE csdl_bit.Posts_tags ADD FOREIGN KEY (id_post) REFERENCES csdl_bit.Posts (id_post);
+ALTER TABLE csdl_bit.Posts_tags ADD FOREIGN KEY (post_id) REFERENCES csdl_bit.Posts (post_id);
 
-ALTER TABLE csdl_bit.Posts_tags ADD FOREIGN KEY (id_tag) REFERENCES  csdl_bit.Tags (id_tag);
+ALTER TABLE csdl_bit.Posts_tags ADD FOREIGN KEY (tag_id) REFERENCES  csdl_bit.Tags (tag_id);
 
-ALTER TABLE csdl_bit.Documents_tags ADD FOREIGN KEY (id_document) REFERENCES csdl_bit.Documents (id_document);
+ALTER TABLE csdl_bit.Documents_tags ADD FOREIGN KEY (document_id) REFERENCES csdl_bit.Documents (document_id);
 
-ALTER TABLE csdl_bit.Documents_tags ADD FOREIGN KEY (id_tag) REFERENCES  csdl_bit.Tags (id_tag);
+ALTER TABLE csdl_bit.Documents_tags ADD FOREIGN KEY (tag_id) REFERENCES  csdl_bit.Tags (tag_id);
 
-ALTER TABLE csdl_bit.Bookmarks ADD FOREIGN KEY (id_document) REFERENCES csdl_bit.Documents (id_document);
+ALTER TABLE csdl_bit.Bookmarks ADD FOREIGN KEY (document_id) REFERENCES csdl_bit.Documents (document_id);
 
-ALTER TABLE csdl_bit.Bookmarks ADD FOREIGN KEY (id_user) REFERENCES csdl_bit.Users (id_user);
+ALTER TABLE csdl_bit.Bookmarks ADD FOREIGN KEY (user_id) REFERENCES csdl_bit.Users (id);
